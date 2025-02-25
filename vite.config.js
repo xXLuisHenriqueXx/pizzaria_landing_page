396fdd0386a1362a78118c241407710afb3ba12e
Luis Henrique Cardoso <127408909+xXLuisHenriqueXx@users.noreply.github.com>
2025-02-25 12:26:30 -0300

    FIX: minor fixes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/pizzaria_landing_page/",
+  base: "https://xxluishenriquexx.github.io/Pizzaria_LandingPage/",
 });
